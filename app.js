56e956e0943277b2f31c6847f088483978f8940d
Marcus Kida <marcus@kida.io>
2017-02-01 16:48:07 +1100

    Add more logging

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 
 var socket = require('socket.io-client')('https://my.lcative.io');
 
+console.log('Connecting to my.locative.io…');
 socket.on('connect', function () {
-  console.log('Connected to my.lcative.io...');
+  console.log('Connected to my.locative.io…');
 
   socket.on('session', function (data) {
-    console.log(data);
+    console.log('Got Session:', data);
   });
 
+  console.log('Authenticating…');
   socket.emit('session', {username: 'YOUR_USERNAME', password:'YOUR_PASSWORD'});
 });
 
